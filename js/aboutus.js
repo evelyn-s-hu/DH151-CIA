55f838472c2ea30977791517f80e97be68c01899
Laura Lu <lauralu@Lauras-MacBook-Pro-2.local>
2022-04-27 04:50:56 -0700

    set up the backend

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,46 +1,113 @@
-// console.log("called")
-// let data = [
-//     {
-//       title: "Audrey",
-//       lat: 30.2655,
-//       lon: 120.1536,
-//     },
+// Global variables
+let map;
+let lat = 0;
+let lon = 0;
+let zl = 3;
+let path = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
+let markers = L.featureGroup();
+let csvdata;
+let lastdate;
 
-//     {
-//         title: "Laura Lu",
-//         lat: 30.2655,
-//         lon: 120.1536,
-//       }
-// ]
-// var map = L.map("map").setView([3, 3], 10);
+// initialize
+$( document ).ready(function() {
+	createMap(lat,lon,zl);
+	readCSV(path);
+});
 
-// L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-//     className: 'map-tiles'
-// }).addTo(map);
+// create the map
+function createMap(lat,lon,zl){
+	map = L.map('map').setView([lat,lon], zl);
 
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+}
 
-// // function to fly to a location by a given id number
-// function flyToIndex(index){
-// 	map.flyTo([data[index].lat,data[index].lon],12)
-// }
+// function to read csv data
+function readCSV(){
+	Papa.parse(path, {
+		header: true,
+		download: true,
+		complete: function(data) {
+			console.log(data);
+			// put the data in a global variable
+			csvdata = data;
 
-// // loop through data
-// data.forEach(function(item){
-// 	// add marker to map
-//     console.log("hello")
-// 	L.marker([item.lat,item.lon]).addTo(map)
-// 		.bindPopup(item.title)
-//         .openPopup();	
-// })
+			// get the last date
+			lastdate = csvdata.meta.fields[csvdata.meta.fields.length-1];
+			
+			// map the data
+			mapCSV(lastdate);
 
+			// create sidebar buttons
+			createSidebarButtons();
 
-var map = L.map('map').setView([34.0697,-118.4432], 17);
+		}
+	});
+}
 
-		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-		}).addTo(map);
+function mapCSV(date){
 
-		var marker = L.marker([34.0697,-118.4432]).addTo(map)
-				.bindPopup('The Technology Sandbox<br> Where Yoh is sitting this very moment')
-				.openPopup();
+	// clear layers
+	markers.clearLayers();
+
+	// update the sidebar title with date
+	$('.sidebar-title').html(date);
+
+	// loop through each entry
+	csvdata.data.forEach(function(item,index){
+		if(item.Lat != undefined){
+			// circle options
+			let circleOptions = {
+				radius: getRadiusSize(item[date]),
+				weight: 1,
+				color: 'white',
+				fillColor: 'red',
+				fillOpacity: 0.5
+			}
+			let marker = L.circleMarker([item.Lat,item.Long],circleOptions)
+			.on('mouseover',function(){
+				this.bindPopup(`${item['Country/Region']}<br>Total confirmed cases as of ${date}: ${item[date]}`).openPopup()
+			})
+			markers.addLayer(marker)	
+		}
+	});
+
+	markers.addTo(map)
+	map.fitBounds(markers.getBounds())
+
+}
+
+function getRadiusSize(value){
+
+    // create empty array to store data
+	let values = [];
+
+	// add case counts for most recent date to the array
+	csvdata.data.forEach(function(item,index){
+		if(item[lastdate] != undefined){
+			values.push(Number(item[lastdate]))
+		}
+	})
+    
+    // get the max case count for most recent date
+	let max = Math.max(...values)
+	
+	// per pixel if 100 pixel is the max range
+	perpixel = max/100;
+
+    // return the pixel size for given value
+	return value/perpixel
+}
+
+function createSidebarButtons(){
+
+	// put all available dates into an array
+	// using slice to remove first 4 columns which are not dates
+	let dates = csvdata.meta.fields.slice(4)
+
+	// loop through each date and create a hover-able button
+	dates.forEach(function(item,index){
+		$('.sidebar-content').append(`<span onmouseover="mapCSV('${item}')" class="sidebar-item" title="${item}">●</span>`)
+	})
+}
---
+++ js/maproom.js
@@ -0,0 +1,129 @@
+// Global variables
+let map;
+let lat = 0;
+let lon = 0;
+let zl = 3;
+let path = "data/bias.csv";
+// let markers = L.featureGroup();
+let csvdata;
+let lastdate;
+let alldata = [];
+let race = []; //go thru less data this way each time
+let racefilter = []; //each city has a total count of the cases, count for each anti race
+
+//FeatureGroup for each layer
+let antiRace = L.featureGroup();
+let antiGender = L.featureGroup();
+let antiAble = L.featureGroup();
+let antiReligion = L.featureGroup();
+let antiSex = L.featureGroup();
+
+// define layers
+let layers = {
+  "Anti Race": antiRace,
+  "Anti Gender": antiReligion,
+};
+
+// initialize
+$(document).ready(function () {
+  createMap(lat, lon, zl);
+  //   readCSV(path);
+  //   plotMap();
+});
+
+// create the map
+function createMap(lat, lon, zl) {
+  map = L.map("map").setView([lat, lon], zl);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.control.layers(null, layers).addTo(map);
+}
+
+// function to read csv data
+function readCSV() {
+  Papa.parse(path, {
+    header: true,
+    download: true,
+    complete: function (data) {
+      // console.log(data);
+      // put the data in a global variable
+      csvdata = data;
+
+      csvdata.data.forEach(function (item, index) {
+        if (item.lat != undefined) {
+          alldata.push(item);
+        }
+      });
+      //get list of unique city names
+      //   const unique = [...new Set(alldata.map((item) => item.city))];
+      let unique = [
+        ...new Map(alldata.map((item) => [item.city, item])).values(),
+      ];
+      console.log(unique);
+
+      //initialize the values
+      unique.forEach(function (item, index) {
+        let obj = {};
+        obj["city"] = item.city;
+        obj["total"] = 0;
+        obj["black"] = 0;
+        obj["amind"] = 0;
+        obj["asian"] = 0;
+        obj["mult"] = 0;
+        obj["hisp"] = 0;
+        obj["lat"] = parseFloat(item.lat);
+        obj["long"] = parseFloat(item.lng);
+        racefilter.push(obj); //add it
+      });
+
+      csvdata.data.forEach(function (item, index) {
+        //find index of the city in racefilter
+        const number = racefilter.findIndex((x) => x.city === item.city);
+
+        if (item["BIAS_DESC_1"] == "Anti-Black or African American") {
+          racefilter[number].black++; //add 1 to the total number of anti black cases
+          racefilter[number].total++;
+        } else if (
+          item["BIAS_DESC_1"] == "Anti-American Indian or Alaska Native"
+        ) {
+          racefilter[number].amind++; //add 1 to the total number of anti american indian cases
+          racefilter[number].total++;
+        } else if (item["BIAS_DESC_1"] == "Anti Asian") {
+          racefilter[number].asian++; //add 1 to the total number of anti asian cases
+          racefilter[number].total++;
+        } else if (item["BIAS_DESC_1"] == "Anti-Multiple Races, Group") {
+          racefilter[number].mult++; //add 1 to the total number of anti mult races cases
+          racefilter[number].total++;
+        } else if (item["BIAS_DESC_1"] == "Anti-Hispanic or Latino") {
+          racefilter[number].hisp++;
+          racefilter[number].total++;
+        }
+      });
+    },
+  });
+}
+
+function plotMap() {
+  // loop through data
+
+  console.log("i'm plotting right now!");
+  console.log(racefilter);
+  racefilter.forEach(function (item) {
+    // create marker
+    let marker = L.marker([item.lat, item.long])
+      .bindPopup(
+        `<h3>${item.city}</h3><p><strong>Anti Asian: </strong>${item.asian}</p><p><strong>Anti-Black or African American: </strong>${item.black}</p><p><strong>Anti-American Indian or Alaska Native: </strong>${item.amind}</p>`
+      )
+      .openPopup();
+
+    // add marker to featuregroup
+    antiRace.addLayer(marker);
+  });
+
+  // after loop, add the FeatureGroup to map
+  antiRace.addTo(map);
+}
